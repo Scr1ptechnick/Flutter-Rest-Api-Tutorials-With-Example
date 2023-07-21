7ba57721b415f2c09b5342405c9dad663959ffd1
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2023-07-21 11:06:59 +0500

    drop down example added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:apitutorials/home_screen.dart';
 import 'package:apitutorials/practice.dart';
 import 'package:flutter/material.dart';
 
+import 'getapi/drop_down_api.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: const HomScreen(),
+      home: const DropDownApi(),
     );
   }
 }
